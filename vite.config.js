74ca84693733e0d26a6bc11d2ed3f54d7b1ebbfa
nikita <nikondi@narod.ru>
2025-06-05 13:17:42 +0300

    Clear welcome page

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
@@ -7,5 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
+    ]
 });
